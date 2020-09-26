be39e451c17ce841cc421119e2d33970ebeff6f8
Anikynetix <anikynetix@gmail.com>
2020-09-26 18:21:44 -0400

    Test cases for start/end connection

---
+++ tester.py
@@ -0,0 +1,40 @@
+'''
+Created on Sep 26, 2020
+
+@author: Anik
+'''
+
+import unittest
+import main
+import mcrcon
+
+
+class Test(unittest.TestCase):
+
+    def test_startCon_1(self):
+
+        rcon = mcrcon.MCRcon("", "", 0)
+
+        try:
+            main.startCon(rcon)
+        except Exception:
+            self.fail()
+
+    def test_startCon_2(self):
+
+        rcon = mcrcon.MCRcon("", "", 0)
+        self.assertEquals(main.startCon(rcon), None)
+
+    def test_endCon_1(self):
+
+        rcon = mcrcon.MCRcon("", "", 0)
+
+        try:
+            main.endCon(rcon)
+        except Exception:
+            self.fail()
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
